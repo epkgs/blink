ddf22518e095f2df69cd523a73d850db1ce18c3d
edwinhuish <edwinhuish@gmail.com>
2024-06-14 22:48:03 +0800

    转移未使用函数到utils

--- view.go
+++ view.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"unsafe"
@@ -675,45 +673,4 @@ func (v *View) SaveWebFrameToPDF(frameId WkeWebFrameHandle, writer io.Writer, wi
 	}
 
 	return nil
-}
-
-// 使用索引格式化文件名
-func getFilePathWithIndex(originalPath string, index int) string {
-	base := filepath.Base(originalPath)
-	dir := filepath.Dir(originalPath)
-	ext := filepath.Ext(base)
-	baseWithoutExt := base[:len(base)-len(ext)]
-
-	return filepath.Join(dir, fmt.Sprintf("%s-%d%s", baseWithoutExt, index, ext))
-}
-
-// 检查文件是否存在，并返回一个未使用的文件路径
-func getUnusedPath(originalPath string) string {
-
-	// 检查文件是否存在
-	if _, err := os.Stat(originalPath); os.IsNotExist(err) {
-		// 文件不存在，返回新路径
-		return originalPath
-	}
-
-	base := filepath.Base(originalPath)
-	dir := filepath.Dir(originalPath)
-	ext := filepath.Ext(base)
-	baseWithoutExt := base[:len(base)-len(ext)]
-
-	index := 1
-	for {
-		// 构造新的文件名
-		newBase := fmt.Sprintf("%s(%d)%s", baseWithoutExt, index, ext)
-		newPath := filepath.Join(dir, newBase)
-
-		// 检查文件是否存在
-		if _, err := os.Stat(newPath); os.IsNotExist(err) {
-			// 文件不存在，返回新路径
-			return newPath
-		}
-
-		// 文件存在，增加索引并重试
-		index++
-	}
 }
