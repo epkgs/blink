899e996c607d32ce7ceda1d170b8b52d27a985b2
edwinhuish <edwinhuish@gmail.com>
2024-08-15 10:39:05 +0800

    alert 增加 info warning

--- pkg/alert/msgbox.go
+++ pkg/alert/msgbox.go
@@ -19,20 +19,25 @@ func messageBox(hwnd uintptr, title, content string, flag uint32) int32 {
 	return win.MessageBox(win.HWND(hwnd), strToWcharPtr(content), strToWcharPtr(title), flag)
 }
 
-func Alert(flags uint32, titleOrContent string, content ...string) int32 {
-	var title string
-	var txt string
-	if len(content) == 0 {
-		title = "错误"
-		txt = titleOrContent
+func pick(titleOrContent string, contents ...string) (title string, content string) {
+	if len(contents) == 0 {
+		return "错误", titleOrContent
 	} else {
-		title = titleOrContent
-		txt = strings.Join(content, "\n")
+		return titleOrContent, strings.Join(contents, "\r\n")
 	}
-
-	return messageBox(0, title, txt, flags)
 }
 
-func Error(titleOrContent string, content ...string) int32 {
-	return Alert(win.MB_OK|win.MB_ICONERROR, titleOrContent, content...)
+func Error(titleOrContent string, contents ...string) int32 {
+	title, content := pick(titleOrContent, contents...)
+	return messageBox(0, title, content, win.MB_OK|win.MB_ICONERROR)
+}
+
+func Info(titleOrContent string, contents ...string) int32 {
+	title, content := pick(titleOrContent, contents...)
+	return messageBox(0, title, content, win.MB_OK|win.MB_ICONINFORMATION)
+}
+
+func Warning(titleOrContent string, contents ...string) int32 {
+	title, content := pick(titleOrContent, contents...)
+	return messageBox(0, title, content, win.MB_OK|win.MB_ICONWARNING)
 }
