8d32c2c23ea3df5ed71ba5300691623159f66adf
edwinhuish <edwinhuish@gmail.com>
2024-08-15 10:48:39 +0800

    添加默认title

--- pkg/alert/msgbox.go
+++ pkg/alert/msgbox.go
@@ -19,25 +19,25 @@ func messageBox(hwnd uintptr, title, content string, flag uint32) int32 {
 	return win.MessageBox(win.HWND(hwnd), strToWcharPtr(content), strToWcharPtr(title), flag)
 }
 
-func pick(titleOrContent string, contents ...string) (title string, content string) {
+func pick(defaultTitle string, titleOrContent string, contents ...string) (title string, content string) {
 	if len(contents) == 0 {
-		return "错误", titleOrContent
+		return defaultTitle, titleOrContent
 	} else {
 		return titleOrContent, strings.Join(contents, "\r\n")
 	}
 }
 
 func Error(titleOrContent string, contents ...string) int32 {
-	title, content := pick(titleOrContent, contents...)
+	title, content := pick("错误", titleOrContent, contents...)
 	return messageBox(0, title, content, win.MB_OK|win.MB_ICONERROR)
 }
 
 func Info(titleOrContent string, contents ...string) int32 {
-	title, content := pick(titleOrContent, contents...)
+	title, content := pick("提示", titleOrContent, contents...)
 	return messageBox(0, title, content, win.MB_OK|win.MB_ICONINFORMATION)
 }
 
 func Warning(titleOrContent string, contents ...string) int32 {
-	title, content := pick(titleOrContent, contents...)
+	title, content := pick("警告", titleOrContent, contents...)
 	return messageBox(0, title, content, win.MB_OK|win.MB_ICONWARNING)
 }
