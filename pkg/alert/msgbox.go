004721714171457a819441dd651453f7e119be9d
池边树下 <mzky@163.com>
2024-08-20 10:28:29 +0800

    弹窗置顶

--- pkg/alert/msgbox.go
+++ pkg/alert/msgbox.go
@@ -1,10 +1,9 @@
 package alert
 
 import (
+	"github.com/lxn/win"
 	"strings"
 	"syscall"
-
-	"github.com/lxn/win"
 )
 
 func strToWcharPtr(s string) *uint16 {
@@ -12,8 +11,10 @@ func strToWcharPtr(s string) *uint16 {
 	return ptr
 }
 
-func messageBox(hwnd uintptr, title, content string, flag uint32) int32 {
-	return win.MessageBox(win.HWND(hwnd), strToWcharPtr(content), strToWcharPtr(title), flag)
+func messageBox(title, content string, flag uint32) int32 {
+	hwnd := win.GetActiveWindow() // 当前激活的窗口
+	win.SetForegroundWindow(hwnd) // 弹窗置于目标窗口之上
+	return win.MessageBox(hwnd, strToWcharPtr(content), strToWcharPtr(title), flag)
 }
 
 func pick(defaultTitle string, titleOrContent string, contents ...string) (title string, content string) {
@@ -26,15 +27,15 @@ func pick(defaultTitle string, titleOrContent string, contents ...string) (title
 
 func Error(titleOrContent string, contents ...string) int32 {
 	title, content := pick("错误", titleOrContent, contents...)
-	return messageBox(0, title, content, win.MB_OK|win.MB_ICONERROR)
+	return messageBox(title, content, win.MB_OK|win.MB_ICONERROR)
 }
 
 func Info(titleOrContent string, contents ...string) int32 {
 	title, content := pick("提示", titleOrContent, contents...)
-	return messageBox(0, title, content, win.MB_OK|win.MB_ICONINFORMATION)
+	return messageBox(title, content, win.MB_OK|win.MB_ICONINFORMATION)
 }
 
 func Warning(titleOrContent string, contents ...string) int32 {
 	title, content := pick("警告", titleOrContent, contents...)
-	return messageBox(0, title, content, win.MB_OK|win.MB_ICONWARNING)
+	return messageBox(title, content, win.MB_OK|win.MB_ICONWARNING)
 }
